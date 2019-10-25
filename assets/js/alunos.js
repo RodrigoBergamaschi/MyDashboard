2f98116f176ebe7fdd1ebd0e3166354149edcd32
RodrigoBergamaschi <54110515+RodrigoBergamaschi@users.noreply.github.com>
2019-10-24 21:41:10 -0300

    24/10

--- assets/js/alunos.js
+++ assets/js/alunos.js
@@ -1,57 +1,93 @@
 var aluno = {};
-var result;
+var listaAlunos = [];
+var totalAlunos = 0;
 
-var alunos = {};
-function adicionarAluno(){
-    aluno.nome = $("#nomeAluno").val();
-    aluno.nota1 = parseInt($("#primeiroBi").val());
-    aluno.nota2 = parseInt($("#segundoBi").val());
-    aluno.nota3 = parseInt($("#terceiroBi").val());
-    aluno.nota4 = parseInt($("#quartoBi").val());
-    aluno.total = calcularTotal(aluno.nota1, aluno.nota2, aluno.nota3, aluno.nota4);
-    aluno.media = calcularMedia(aluno.total);
-    aluno.situacao = calcularSituacao(aluno.media);
-    console.log($(":form").valid());
-    console.log($(":form").validate());
-    
-    adicionarLinhaTabela(aluno);
-
-    aluno = {};
-   $(":input").val('');
+function validarForm() {
+    return $('#nomeAluno').valid() &&
+           $("#primeiroBi").valid() &&
+           $("#segundoBi").valid() &&
+           $("#terceiroBi").valid() &&
+           $("#quartoBi").valid();
 }
 
-function calcularTotal(nota1, nota2, nota3, nota4){
-    return nota1 + nota2 + nota3 +nota4;
+function feedbackFormValidacao() {
+
+    if ($('#nomeAluno').valid()) {
+        $('#nomeAluno').removeClass('is-invalid');
+    } else {
+        $('#nomeAluno').addClass('is-invalid');
+    }
 }
 
-function calcularMedia(total){
-  return total / 4;
+function adicionarAluno() {
+
+    var formValido = validarForm();
+
+    if (formValido) {
+        aluno.nome = $("#nomeAluno").val();
+        aluno.nota1 = parseInt($("#primeiroBi").val());
+        aluno.nota2 = parseInt($("#segundoBi").val());
+        aluno.nota3 = parseInt($("#terceiroBi").val());
+        aluno.nota4 = parseInt($("#quartoBi").val());
+        aluno.total = calcularTotal(aluno.nota1, aluno.nota2, aluno.nota3, aluno.nota4);
+        aluno.media = calcularMedia(aluno.total);
+        aluno.situacao = calcularSituacao(aluno.media);
+
+        adicionarLinhaTabela(aluno);
+        
+        aluno = {};
+       
+        $(":input").val("");
+
+    } else {
+        feedbackFormValidacao();
+    }
 }
 
-function calcularSituacao(media){
+function calcularTotal(nota1, nota2, nota3, nota4) {
+    return nota1 + nota2 + nota3 + nota4;
+}
+
+function calcularMedia(valor) {
+    return valor / 4;
+}
+
+function calcularSituacao(media) {
     const MEDIA_APROVACAO = 7;
 
-    if(media >= MEDIA_APROVACAO){
-        
-        return '<span class="badge badge-success">APROVADO</span>';
-
-    }
-    else{
-        
-        return '<span class="badge badge-danger">REPROVADO</span>';
+    if (media >= MEDIA_APROVACAO) {
+        return "<span class='badge badge-success'>APROVADO</span>";
+    } else {
+        return "<span class='badge badge-danger'>REPROVADO</span>";
     }
 }
 
-function adicionarLinhaTabela(objeto){
-    console.log(objeto);
+function adicionarLinhaTabela(objeto) {
     var tabela = document.querySelector("table");
-    var tr = tabela.insertRow(1);
+    var tr = tabela.insertRow();
     tr.innerHTML = "<td>" + objeto.nome + "</td>" +
-                   "<td>" + objeto.nota1 + "</td>" +
-                   "<td>" + objeto.nota2 + "</td>" +
-                   "<td>" + objeto.nota3 + "</td>" +
-                   "<td>" + objeto.nota4 + "</td>" +
-                   "<td>" + objeto.total + "</td>" +
-                   "<td>" + objeto.media + "</td>" +
-                   "<td>" + objeto.situacao + "</td>"; 
+        "<td>" + objeto.nota1 + "</td>" +
+        "<td>" + objeto.nota2 + "</td>" +
+        "<td>" + objeto.nota3 + "</td>" +
+        "<td>" + objeto.nota4 + "</td>" +
+        "<td>" + objeto.total + "</td>" +
+        "<td>" + objeto.media + "</td>" +
+        "<td>" + objeto.situacao + "</td>"+
+        "<td><button type='button' class='btn btn-danger btn-xs' onclick='deletarLinha(this)'><i class='fas fa-user-times' ></i></button></td>";
+        toastr.success('Aluno incluido com sucesso!');
 }
+
+function deletarLinha(linha) {
+    var i = linha.parentNode.parentNode.rowIndex;
+    document.querySelector('table').deleteRow(i);
+    toastr.info('Aluno excluído com sucesso!');
+    
+}
+
+function totalDeAlunos(valor)
+{
+}
+
+
+   
+//document.getElementById('tableAlunos').deleteRow(index)
